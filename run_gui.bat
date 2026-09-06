b1c240e5bb853c5f0cd6bc3c0cf8414e77e05052
Xiangming <40314529+vguangshen@users.noreply.github.com>
2026-09-06 23:40:14 +0800

    v1.1.2：默认GUI显示底部更新入口

--- run_gui.bat
+++ run_gui.bat
@@ -3,7 +3,6 @@ chcp 65001 >nul
 setlocal
 
 set "JAVA="
-rem 默认启动 GitHub 自动更新启动层；旧界面可使用 run_gui_legacy.bat 回退
 if exist "%~dp0jre\bin\javaw.exe" set "JAVA=%~dp0jre\bin\javaw.exe"
 if not defined JAVA if exist "%~dp0jre\bin\java.exe" set "JAVA=%~dp0jre\bin\java.exe"
 if not defined JAVA where javaw >nul 2>nul && set "JAVA=javaw"
@@ -24,4 +23,4 @@ if not exist "%~dp0LicenseRecoverOverlay.jar" (
   exit /b 1
 )
 
-start "" "%JAVA%" -Dfile.encoding=UTF-8 -Dsun.java2d.dpiaware=true -Dsun.java2d.noddraw=true -cp "%~dp0LicenseRecoverOverlay.jar;%~dp0LicenseRecoverGUI.jar" LicenseRecoverModernGUILauncher %*
+start "" "%JAVA%" -Dfile.encoding=UTF-8 -Dsun.java2d.dpiaware=true -Dsun.java2d.noddraw=true -cp "%~dp0LicenseRecoverOverlay.jar;%~dp0LicenseRecoverGUI.jar" LicenseRecoverModernGUIUiPatchLauncher %*
